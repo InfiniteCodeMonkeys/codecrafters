0410ec3d013b9eb8ae0b0341011307db6bb8f6b6
Mike Dyer <mike@chindogulabs.com>
2025-07-29 11:21:49 -0600

    start nn

---
+++ neural-net/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"math"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+// neuralNet contains all of the information
+// that defines a trained neural network.
+type neuralNet struct {
+	config  neuralNetConfig
+	wHidden *mat.Dense
+	bHidden *mat.Dense
+	wOut    *mat.Dense
+	bOut    *mat.Dense
+}
+
+// neuralNetConfig defines our neural network
+// architecture and learning parameters.
+type neuralNetConfig struct {
+	inputNeurons  int
+	outputNeurons int
+	hiddenNeurons int
+	numEpochs     int
+	learningRate  float64
+}
+
+// newNetwork initializes a new neural network.
+func newNetwork(config neuralNetConfig) *neuralNet {
+	return &neuralNet{config: config}
+}
+
+// sigmoid implements the sigmoid function
+// for use in activation functions.
+func sigmoid(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-x))
+}
+
+// sigmoidPrime implements the derivative
+// of the sigmoid function for backpropagation.
+func sigmoidPrime(x float64) float64 {
+	return sigmoid(x) * (1.0 - sigmoid(x))
+}
