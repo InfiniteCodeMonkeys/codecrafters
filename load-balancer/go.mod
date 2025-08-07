21230f4d008c7b1fd56fb4a02a4779103c2f6398
Mike Dyer <mike@chindogulabs.com>
2025-08-06 20:08:50 -0600

    start lb

---
+++ load-balancer/go.mod
@@ -0,0 +1,3 @@
+module github.com/InfiniteCodeMonkeys/go-load-balancer
+
+go 1.23.4
---
+++ load-balancer/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+	"sync/atomic"
+)
+
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
+
+func main() {
+	url, _ := url.Parse("http://localhost:8080")
+	reverseProxy := httputil.NewSingleHostReverseProxy(url)
+
+	http.HandleFunc("/", reverseProxy.ServeHTTP)
+}
+
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, 1) % uint64(len(s.backends)))
+}
+
+// GetNextPeer returns next active peer to take a connection
+func (s *ServerPool) GetNextPeer() *Backend {
+	// loop entire backends to find out an Alive backend
+	next := s.NextIndex()
+	l := len(s.backends) + next // start from next and move a full cycle
+	for i := next; i < l; i++ {
+		idx := i % len(s.backends) // take an index by modding with length
+		// if we have an alive backend, use it and store if its not the original one
+		if s.backends[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx)) // mark the current one
+			}
+			return s.backends[idx]
+		}
+	}
+	return nil
+}
